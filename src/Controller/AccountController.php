cecda235c54d77b3f257146bda04d341b9e80605
Flavien <livinien@hotmail.fr>
2023-02-23 17:44:18 +0100

    Mise en place de symfony + inscription + connexion + page de compte de l'utilisateur

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountController extends AbstractController
+{
+
+    // ACCEDER AU COMPTE DE L'UTILISATEUR
+    
+    #[Route('/compte', name: 'app_account')]
+    public function index(): Response
+    {
+        return $this->render('account/index.html.twig');
+    }
+}
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+
+// CREATION DU FORMULAIRE D'INSCRIPTION DE L'UTILISATEUR
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 60
+                ]),
+                'attr' => [
+                    'placeholder' => 'Entre votre adresse email'
+                ]
+            ])
+            //->add('roles')
+            ->add('password', RepeatedType::class, [
+                'type'=> PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.', 
+                'label' => "Mot de Passe",
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Entrez votre mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Saissisez une seconde fois votre mot de passe'
+                    ]
+                ],
+            ])
+            
+            
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
